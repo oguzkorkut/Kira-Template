62f9ca81e548de3385481339792aad09a147c430
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-07 14:54:26 +0300

    Kullanıcı ekleme sayfası ile ilgili düzenleme yapılıyor

--- src/app/pages/customer/customer-create/customer-create.component.ts
+++ src/app/pages/customer/customer-create/customer-create.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../../entity/user';
+import { CONSTANTS } from '../../../service/constants';
+import { NotificationsService } from 'angular2-notifications';
+import { KiraService } from '../../../service/kira.service';
+import { PhoneFilterPipe } from '../../../pipe/phone-filter.pipe';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-customer-create',
@@ -22,12 +28,67 @@ export class CustomerCreateComponent implements OnInit {
 
   user: User;
 
-  constructor() { }
+  public personelInfoForm: FormGroup;
+  public communicationInfoForm: FormGroup;
+  public systemInfoForm: FormGroup;
+
+  constructor(private  phoneFilter: PhoneFilterPipe, 
+              private kiraService: KiraService, 
+              private notificationsService: NotificationsService) { 
+
+      this.mask = CONSTANTS.phoneMask;
+  }
+
 
   ngOnInit() {
-
     this.user = new User;
+
+    this.createPersonelInfoFormGroup();
+    this.createCommunicationInfoFormGroup();
+    this.createSystemInfoFormGroup();
+
   }
+
+
+  createPersonelInfoFormGroup(){
+    const firstname= new FormControl('', Validators.required);
+    const surname= new FormControl('', Validators.required);
+    const tckn= new FormControl('', [Validators.required, CustomValidators.gt(10000000000)]);
+    const vkn= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
+
+    this.personelInfoForm = new FormGroup({
+        firstname: firstname,
+        surname: surname,
+        tckn: tckn,
+        vkn: vkn
+    });
+}
+
+createCommunicationInfoFormGroup(){
+  const phone= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
+  const email= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
+
+  this.communicationInfoForm = new FormGroup({
+      phone: phone,
+      email: email
+  });
+}
+
+createSystemInfoFormGroup(){
+  const username= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
+  const profession= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
+  const password= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
+  const repassword= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
+  const smscode= new FormControl('', Validators.required);
+
+  this.systemInfoForm = new FormGroup({
+      username: username,
+      profession: profession,
+      password: password,
+      repassword: repassword,
+      smscode: smscode
+  });
+}
 
   cancel(){
 
