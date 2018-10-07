8dcf12cad6cb1e7883bf36dc7f4d8a7e32f15aa2
Oğuz Korkut <oguzkrkt@gmail.com>
2018-10-07 15:54:34 +0300

    Kullanıcı ekleme sayfa tasarımı yapıldı.

--- src/app/pages/customer/customer-create/customer-create.component.ts
+++ src/app/pages/customer/customer-create/customer-create.component.ts
@@ -6,6 +6,8 @@ import { KiraService } from '../../../service/kira.service';
 import { PhoneFilterPipe } from '../../../pipe/phone-filter.pipe';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CustomValidators } from 'ng2-validation';
+import { ReturnModel } from '../../../entity/ReturnModel';
+import { Profession } from '../../../entity/profession';
 
 @Component({
   selector: 'app-customer-create',
@@ -28,6 +30,8 @@ export class CustomerCreateComponent implements OnInit {
 
   user: User;
 
+  professions: Profession[] = [];
+
   public personelInfoForm: FormGroup;
   public communicationInfoForm: FormGroup;
   public systemInfoForm: FormGroup;
@@ -47,6 +51,7 @@ export class CustomerCreateComponent implements OnInit {
     this.createCommunicationInfoFormGroup();
     this.createSystemInfoFormGroup();
 
+    this.getProfessions();
   }
 
 
@@ -54,7 +59,7 @@ export class CustomerCreateComponent implements OnInit {
     const firstname= new FormControl('', Validators.required);
     const surname= new FormControl('', Validators.required);
     const tckn= new FormControl('', [Validators.required, CustomValidators.gt(10000000000)]);
-    const vkn= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
+    const vkn= new FormControl('', CustomValidators.gt(0));
 
     this.personelInfoForm = new FormGroup({
         firstname: firstname,
@@ -65,8 +70,10 @@ export class CustomerCreateComponent implements OnInit {
 }
 
 createCommunicationInfoFormGroup(){
-  const phone= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
-  const email= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
+  //this.phoneFilter.transform(val.phone)
+
+  const phone= new FormControl('', Validators.required);
+  const email = new FormControl('', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]);
 
   this.communicationInfoForm = new FormGroup({
       phone: phone,
@@ -75,11 +82,11 @@ createCommunicationInfoFormGroup(){
 }
 
 createSystemInfoFormGroup(){
-  const username= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
-  const profession= new FormControl('', [Validators.required, CustomValidators.gt(0)]);
-  const password= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
-  const repassword= new FormControl('', [Validators.required, CustomValidators.gt(-1)]);
-  const smscode= new FormControl('', Validators.required);
+  const username= new FormControl('', Validators.required);
+  const profession= new FormControl('', Validators.required);
+  const password= new FormControl('', Validators.required);
+  const repassword = new FormControl('', [Validators.required, CustomValidators.equalTo(password)]);
+  const smscode= new FormControl('');
 
   this.systemInfoForm = new FormGroup({
       username: username,
@@ -95,7 +102,36 @@ createSystemInfoFormGroup(){
   }
   
   save(){
+    if (this.personelInfoForm.valid 
+      && (this.communicationInfoForm.valid && this.phoneFilter.transform(this.communicationInfoForm.controls.phone.value) != '')
+      && this.systemInfoForm.valid ) {
+      console.log("valid");
+    } else {
+      console.log("invalid");
+    }
 
+  }
+
+
+  getProfessions(): void {
+
+    this.kiraService.getProfessions()
+      .then((res: ReturnModel) => {
+  
+        if (res.status) {
+          this.professions = res.result as Profession[];
+  
+          if(this.professions){
+            //this.notificationsService.success('Bilgi', this.professions.length + " meslek kaydı çekildi.");
+          }
+        } else {
+          this.notificationsService.error('Hata', res.message);
+        }
+      })
+      .catch((res: Response) => {
+          this.notificationsService.error('Hata', res.statusText);
+        }
+      );
   }
 
 }
